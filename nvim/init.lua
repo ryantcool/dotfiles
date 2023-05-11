fa2f4679a5ff6decc85b6036f6e40d1babfa6497
Ryan Cool <ryan.cool@tuta.io>
2023-05-10 23:40:01 -0400

    Added config folder for nvim

---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+require("plugins")
+require("core/options")
+require("core/autocmds")
+require("core/statusline")
+require("core/keymaps")
+require("plugins/nvim-tree")
+require("plugins/indent-blankline")
+require("plugins/coc-nvim")
